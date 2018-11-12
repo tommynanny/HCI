551bbdf725a0546d49b7e4e2c9c3fc54f36f0cf6
Nanlin Sun <nannie@vt.edu>
2018-11-12 17:47:44 -0500

    add legend

---
+++ Assets/LocationMark.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationMark : MonoBehaviour {
+
+	public Animator YellowAnimator;
+	public Animator GreenAnimator;
+	public Animator BlueAnimator;
+	public Animator WhiteAnimator;
+	// Use this for initialization
+	public void LocateYellow()
+	{
+		ClearAll();
+		YellowAnimator.SetBool("Flash", true);
+	}
+
+	public void LocateWhite()
+	{
+		ClearAll();
+		WhiteAnimator.SetBool("Flash", true);
+	}
+	public void LocateGreen()
+	{
+		ClearAll();
+		GreenAnimator.SetBool("Flash", true);
+	}
+	public void LocateBlue()
+	{
+		ClearAll();
+		BlueAnimator.SetBool("Flash", true);
+	}
+
+	public void ClearAll()
+	{
+		GreenAnimator.SetBool("Flash", false);
+		WhiteAnimator.SetBool("Flash", false);
+		YellowAnimator.SetBool("Flash", false);
+		BlueAnimator.SetBool("Flash", false);
+	}
+
+}
---
+++ Assets/Script/LegendWindowButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegendWindowButton : MonoBehaviour
+{
+	Animator animator;
+	// Use this for initialization
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	public void ShowLegendWindow()
+	{
+		animator.SetBool("Show", true);
+	}
+
+	public void HideLegendWindow()
+	{
+		animator.SetBool("Show", false);
+	}
+}
